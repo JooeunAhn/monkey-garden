526e031328bcde944fa664a2e617d637ef11b478
hyunjoong-getmiso <hyunjoong@getmiso.com>
2017-08-06 00:54:26 +0900

    master

--- monkey_garden/root/views.py
+++ monkey_garden/root/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
+
+# https://www.youtube.com/watch?v=zZ4n0fI23_g
+# www.youtube.com/embed/z7d1fDUyuzQ
+
+def embed(input):
+	pass
+	
 # Create your views here.
 def root(request):
 	return render(request, 'layout.html', {})
-
 
 def mypage(request):
 	return render(request, 'mypage.html', {})
