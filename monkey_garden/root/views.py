fecb533389923c75c101a06ae19f3a02b52e3529
hyunjoong-getmiso <hyunjoong@getmiso.com>
2017-08-06 06:45:52 +0900

    master

--- monkey_garden/root/views.py
+++ monkey_garden/root/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from garden.models import MessageHistory
+from accounts.models import Profile
+import random
 
 
 def root(request):
@@ -8,7 +10,18 @@ def root(request):
 
 
 def mypage(request):
-    message_histories = MessageHistory.objects.exclude(message__url="")
-    message_histories = message_histories.filter(user=request.user).order_by("-created_at")
-    return render(request, 'mypage.html',
-        {"message_histories": message_histories})
+	message_histories = MessageHistory.objects.exclude(message__url="")
+	message_histories = message_histories.filter(user=request.user).order_by("-created_at")
+	profile = Profile.objects.all()
+	randomList=["KKIIII", "KKKKUUUU", "KKIK", "KKAK", "KKKKKIIII", "KKKIIIIUUUI"]
+
+	for mh in message_histories:
+		if mh.message.text == "":
+			mh.message.text = random.choice(randomList)
+			mh.save()
+
+	return render(request, 'mypage.html',
+	{
+		"message_histories": message_histories,
+		"profile": profile,
+	})
