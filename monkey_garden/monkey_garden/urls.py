79fd795ef32e11afae3404acec596efd867ca5c3
JooeunAhn <jflow@hanyang.ac.kr>
2017-08-05 16:08:11 +0900

    add facebook login

--- monkey_garden/monkey_garden/urls.py
+++ monkey_garden/monkey_garden/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from accounts.views import FacebookLogin
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/facebook/$', FacebookLogin.as_view(), name="fb_login"),
 ]
